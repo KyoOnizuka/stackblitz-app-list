15ff64baee26f0dfbb0e017752384e49901295e0
KyoOnizuka <30726685+KyoOnizuka@users.noreply.github.com>
2023-08-04 14:45:43 +0700

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
-import 'zone.js/dist/zone';
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { bootstrapApplication } from '@angular/platform-browser';
+import './polyfills';
+
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
 import { AppModule } from './app/app.module';
 
-bootstrapApplication(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
+  // Ensure Angular destroys itself on hot reloads.
+  if (window['ngRef']) {
+    window['ngRef'].destroy();
+  }
+  window['ngRef'] = ref;
+
+  // Otherwise, log the boot error
+}).catch(err => console.error(err));
